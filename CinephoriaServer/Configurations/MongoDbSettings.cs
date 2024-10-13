96d5be675ae1833433b5c75ed48a9f28c47cc4f5
Patrice SIMO <simopatrice@yahoo.fr>
2024-10-13 18:12:28 +0200

    #381

---
+++ CinephoriaServer/Configurations/MongoDbSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace CinephoriaServer.Configurations
+{
+    public class MongoDbSettings
+    {
+        public string ConnectionString { get; set; } = string.Empty;
+        public string DatabaseName { get; set; } = string.Empty;
+    }
+
+}
