14142ce9422547f6c5250e1f9f93cbbc48d6304f
Patrice SIMO <simopatrice@yahoo.fr>
2025-01-26 00:04:30 +0100

    #589 Organisation des routes

--- WebApp/src/app/core/routes/adminRoute/admin-routing.module.ts
+++ WebApp/src/app/core/routes/adminRoute/admin-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AuthGuard } from '../../guards/auth.guard';
+import { AuthGuard } from '@app/core/guards/auth.guard';
+
 
 const routes: Routes = [
   {
@@ -8,20 +9,20 @@ const routes: Routes = [
     canActivate: [AuthGuard],
     data: { expectedRole: 'Admin, Employee' },
     children: [
-      {
-        path: 'manage-movie',
-        // loadComponent: () => import('../../../pages/adminModule/manage-movie/manage-movie.component').then(m => m.ManageMovieComponent)
-      },
-      {
-        path: 'movie-detail/:id',
-        // loadComponent: () => import('../../../pages/adminModule/manage-movie/movie-detail/movie-detail.component').then(m => m.MovieDetailComponent)
-      }
-    ]
-  }
+      // {
+      //   path: 'manage-movie',
+      //   loadComponent: () => import('@app/pages/adminModule/manage-movie/manage-movie.component').then((m) => m.ManageMovieComponent),
+      // },
+      // {
+      //   path: 'movie-detail/:id',
+      //   loadComponent: () => import('@app/pages/adminModule/manage-movie/movie-detail/movie-detail.component').then((m) => m.MovieDetailComponent),
+      // },
+    ],
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AdminRoutingModule { }
+export class AdminRoutingModule {}
