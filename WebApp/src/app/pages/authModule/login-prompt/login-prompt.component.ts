912558a62d40e1b25ef3a796072b58b56c1aa04b
Patrice SIMO <simopatrice@yahoo.fr>
2025-01-30 15:08:34 +0100

    #601 LoginPromptComponent

---
+++ WebApp/src/app/pages/authModule/login-prompt/login-prompt.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-prompt',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './login-prompt.component.html',
+  styleUrl: './login-prompt.component.scss'
+})
+export class LoginPromptComponent {
+
+}
