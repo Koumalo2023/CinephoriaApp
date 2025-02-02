83f5d903421de9e0b43b4e37c9213187f08e4791
Patrice SIMO <simopatrice@yahoo.fr>
2025-02-02 22:23:32 +0100

    #612 choix des sièges

--- WebApp/src/app/pages/homeModule/reservation/seat-selector/seat-selector.component.ts
+++ WebApp/src/app/pages/homeModule/reservation/seat-selector/seat-selector.component.ts
@@ -1,20 +1,61 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
+import { ReservationService } from '@app/core/services/reservation.service';
+import { SeatDto } from '@app/core/models/seat.models';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-seat-selector',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './seat-selector.component.html',
   styleUrl: './seat-selector.component.scss'
 })
-export class SeatSelectorComponent {
-  @Output() seatsSelected = new EventEmitter<any[]>(); 
+export class SeatSelectorComponent implements OnChanges {
+  @Input() showtimeId: number | null = null;
+  @Input() selectedSeats: SeatDto[] = [];
+  @Output() seatsSelected = new EventEmitter<any[]>();
 
-  selectedSeats: any[] = [];
+  availableSeats: SeatDto[] = [];
+  localSelectedSeats: SeatDto[] = [];
 
-  // Méthode pour sélectionner un siège
-  selectSeat(seat: any) {
-    this.selectedSeats.push(seat);
-    this.seatsSelected.emit(this.selectedSeats);
+  constructor(private reservationService: ReservationService) {}
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['showtimeId'] && this.showtimeId) {
+      this.loadAvailableSeats();
+    }
+    if (changes['selectedSeats'] && this.selectedSeats) {
+      this.localSelectedSeats = [...this.selectedSeats]; 
+    }
   }
+
+  loadAvailableSeats() {
+    if (this.showtimeId) {
+      this.reservationService.getAvailableSeats(this.showtimeId).subscribe({
+        next: (seats) => {
+          this.availableSeats = seats;
+        },
+        error: (err) => {
+          console.error('Erreur lors du chargement des sièges disponibles :', err);
+        }
+      });
+    }
+  }
+
+  selectSeat(seat: SeatDto) {
+    if (!this.localSelectedSeats.includes(seat)) {
+      this.localSelectedSeats.push(seat);
+    } else {
+      this.localSelectedSeats = this.localSelectedSeats.filter(s => s !== seat);
+    }
+  }
+
+  confirmSelection() {
+    if (this.localSelectedSeats.length > 0) {
+      this.seatsSelected.emit(this.localSelectedSeats);
+    } else {
+      alert('Veuillez sélectionner au moins un siège.');
+    }
+  }
+  
 }
