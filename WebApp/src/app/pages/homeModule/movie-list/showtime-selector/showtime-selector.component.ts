b28c4a8e870f128268f0b0b65891c001e7dd72b7
Patrice SIMO <simopatrice@yahoo.fr>
2025-02-02 22:19:00 +0100

    #611 Choix des séances des films

--- WebApp/src/app/pages/homeModule/movie-list/showtime-selector/showtime-selector.component.ts
+++ WebApp/src/app/pages/homeModule/movie-list/showtime-selector/showtime-selector.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
 import { ShowtimeDto } from '@app/core/models/showtime.models';
+import { MovieService } from '@app/core/services/movie.service';
 
 @Component({
   selector: 'app-showtime-selector',
@@ -9,12 +10,36 @@ import { ShowtimeDto } from '@app/core/models/showtime.models';
   templateUrl: './showtime-selector.component.html',
   styleUrl: './showtime-selector.component.scss'
 })
-export class ShowtimeSelectorComponent {
+export class ShowtimeSelectorComponent implements OnChanges {
   @Input() showtimes: ShowtimeDto[] = [];
+  @Input() movieId: number | null = null;
   @Output() showtimeSelected = new EventEmitter<ShowtimeDto>();
 
+  constructor(private movieService: MovieService) {}
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['movieId'] && this.movieId) {
+      this.loadShowtimes();
+    }
+  }
+
+  loadShowtimes() {
+    if (this.movieId) {
+      this.movieService.getMovieSessions(this.movieId).subscribe({
+        next: (showtimes) => {
+          this.showtimes = showtimes;
+        },
+        error: (err) => {
+          console.error('Erreur lors du chargement des séances :', err);
+        }
+      });
+    } else {
+      this.showtimes = [];
+    }
+  }
+  
   onShowtimeSelected(showtime: ShowtimeDto): void {
     console.log('Séance sélectionnée :', showtime);
-    this.showtimeSelected.emit(showtime); // Émet l'événement au parent
+    this.showtimeSelected.emit(showtime);
   }
 }
